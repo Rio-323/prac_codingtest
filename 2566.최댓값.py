b4e8eb8268d7b16adaca9265dfbebe8198953980
Rio-323 <kimsiyeon5214@gmail.com>
2024-06-03 12:43:18 +0900

    5일차 코딩테스트

---
+++ 2566.최댓값.py
@@ -0,0 +1,40 @@
+import sys
+
+# 전체 입력을 한 번에 읽어오기
+data = sys.stdin.read().split()
+
+# 9x9 배열 초기화
+arr = []
+index = 0
+
+for i in range(9):
+    row = list(map(int, data[index:index + 9]))
+    arr.append(row)
+    index += 9
+
+# 최대값과 위치 초기화
+max_value = arr[0][0]
+max_i = 0
+max_j = 0
+
+# 배열 순회하며 최대값과 위치 찾기
+for i in range(9):
+    for j in range(9):
+        if arr[i][j] > max_value:
+            max_value = arr[i][j]
+            max_i = i
+            max_j = j
+
+# 최대값 출력
+print(max_value)
+# 최대값의 위치 출력 (1-indexed)
+print(max_i + 1, max_j + 1)
+
+
+# 최대값만 구하는 방법
+# max_value = max(map(max, arr))
+# print(max_value)
+
+
+
+
