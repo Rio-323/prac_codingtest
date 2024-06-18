0f1f4a775dd3d1cb27a104e7e975ad099bbb6fc7
Rio-323 <kimsiyeon5214@gmail.com>
2024-06-18 20:18:47 +0900

    코딩테스트 연습

--- 14일차/17202.핸드폰번호궁합.py
+++ 14일차/17202.핸드폰번호궁합.py
@@ -17,3 +17,30 @@ while len(love) > 2:
     love = result
 
 print(love)
+
+# 재귀함수 버전
+
+import sys as s
+
+a = s.stdin.readline().rstrip()
+b = s.stdin.readline().rstrip()
+
+def create_love_string(a, b):
+    love = ''
+    for i in range(8):
+        love += a[i]
+        love += b[i]
+    return love
+
+def calculate_love(love):
+    if len(love) == 2:
+        return love
+    else:
+        result = ''
+        for i in range(len(love) - 1):
+            temp = int(love[i]) + int(love[i+1])
+            result += str(temp % 10)
+        return calculate_love(result)
+
+love = create_love_string(a, b)
+print(calculate_love(love))
